21302e1ba8c3128f8e18e490eee91b476b91c706
Va-rx <frytkaspl@gmail.com>
2024-03-07 23:12:16 +0100

    client data model

---
+++ lab_gniazda/domowe/src/ClientAddress.java
@@ -0,0 +1,41 @@
+import java.net.InetAddress;
+import java.util.Objects;
+
+public class ClientAddress {
+    private InetAddress ip;
+    private Integer port;
+
+    public ClientAddress(InetAddress ip, Integer port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public InetAddress getIp() {
+        return ip;
+    }
+
+    public void setIp(InetAddress ip) {
+        this.ip = ip;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientAddress that = (ClientAddress) o;
+        return Objects.equals(ip, that.ip) && Objects.equals(port, that.port);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip, port);
+    }
+}

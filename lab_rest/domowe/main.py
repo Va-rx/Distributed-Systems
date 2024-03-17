51a8c9b0b345148ba6243981c40b67aa516e8c0f
Va-rx <frytkaspl@gmail.com>
2024-03-18 00:41:50 +0100

    end

--- lab_rest/domowe/main.py
+++ lab_rest/domowe/main.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi import FastAPI, Query
 from fastapi.responses import HTMLResponse
 import request
@@ -29,42 +30,51 @@ async def root():
 
 @app.get("/current_weather", response_class=HTMLResponse)
 async def current_weather(city: str = Query(..., title="city")):
-    temp, fltemp, cloud, humidity = request.get_current_weather(city)
-    return f"""
-    <h1>Current weather information for {city}</h1>
-    <p>Temperature: {temp}C</p>
-    <p>Feels like temperature: {fltemp}C</p>
-    <p>Clouds: {cloud}%</p>
-    <p>Humidity: {humidity}</p>
-
-    <form action="/current_weather/extended" method="get">
-    <input type="hidden" name="city" value="{city}">
-    <button type="submit">Extended details</button>
-    </form>
-    """
+    try:
+        temp, fltemp, cloud, humidity = request.get_current_weather(city)
+        return f"""
+        <h1>Current weather information for {city}</h1>
+        <p>Temperature: {temp}C</p>
+        <p>Feels like temperature: {fltemp}C</p>
+        <p>Clouds: {cloud}%</p>
+        <p>Humidity: {humidity}</p>
+    
+        <form action="/current_weather/extended" method="get">
+        <input type="hidden" name="city" value="{city}">
+        <button type="submit">Extended details</button>
+        </form>
+        """
+    except requests.HTTPError as e:
+        return f"<h1>{e}</h1>"
 
 
 @app.get("/current_weather/extended", response_class=HTMLResponse)
 async def current_weather_extended(city: str = Query(..., title="city")):
-    temp_min_c, temp_max_c, is_mud, sunrise, sunset = request.get_current_extended_weather(city)
+    try:
+        temp_min_c, temp_max_c, is_mud, sunrise, sunset = request.get_current_extended_weather(city)
 
-    return f"""
-    <h1>Today's weather information for {city}</h1>
-    <p>Min temperature: {temp_min_c}C</p>
-    <p>Max temperature: {temp_max_c}C</p>
-    <p>Mud: {is_mud}</p>
-    <p>Sunrise: {sunrise}</p>
-    <p>Sunset: {sunset}</p>
-    """
+        return f"""
+        <h1>Today's weather information for {city}</h1>
+        <p>Min temperature: {temp_min_c}C</p>
+        <p>Max temperature: {temp_max_c}C</p>
+        <p>Mud: {is_mud}</p>
+        <p>Sunrise: {sunrise}</p>
+        <p>Sunset: {sunset}</p>
+        """
+    except requests.HTTPError as e:
+        return f"<h1>{e}</h1>"
 
 
 # free api gives access only for days: {1, 2} (tomorrow, after tomorrow)
 @app.get("/forecast_weather", response_class=HTMLResponse)
 async def get_weather_forecast(city: str = Query(...), days: int = Query(1)):
-    hours = request.get_forecast_weather(city, days)
-    result = generate_paragraph(hours)
+    try:
+        hours = request.get_forecast_weather(city, days)
+        result = generate_paragraph(hours)
 
-    return f"""
-    <h1>Forecast weather for {city}</h1>
-    {result}
-    """
+        return f"""
+        <h1>Forecast weather for {city}</h1>
+        {result}
+        """
+    except requests.HTTPError as e:
+        return f"<h1>{e}</h1>"
--- lab_rest/domowe/request.py
+++ lab_rest/domowe/request.py
@@ -1,4 +1,5 @@
-from requests import get, RequestException
+import requests
+from requests import get
 from datetime import datetime, timedelta
 from api_key import key1, key2
 from helper import fahrenheit_to_celsius
@@ -6,15 +7,11 @@ from typing import List
 
 
 def fetch_data(url):
-    try:
-        response = get(url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f"External api request error: {response.status_code}")
-            return None
-    except RequestException as re:
-        print(f"Server error: {re}")
+    response = get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"External api request error: {response.status_code}")
         return None
 
 
@@ -23,37 +20,55 @@ def round_data(data: List[float]):
 
 
 def get_current_weather(city: str):
-    json1 = fetch_data('http://api.weatherapi.com/v1/current.json?key=' + key1 + '&q=' + city + '&aqi=no')
-    json2 = fetch_data('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + city + '?unitGroup=us&key=' + key2 + '&contentType=json')
+    url1 = 'http://api.weatherapi.com/v1/current.json?key=' + key1 + '&q=' + city + '&aqi=no'
+    url2 = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + city + '?unitGroup=us&key=' + key2 + '&contentType=json'
+    json1 = fetch_data(url1)
+    json2 = fetch_data(url2)
+    if json1 is None and json2 is None:
+        raise requests.HTTPError("Unable to catch the data from an API")
+    if json1 is not None and json2 is None:
+        json1_curr = json1["current"]
+        temp_c1 = json1_curr["temp_c"]
+        fltemp_c1 = json1_curr["feelslike_c"]
+        humidity1 = json1_curr["humidity"]
+        cloud1 = json1_curr["cloud"]
+        return temp_c1, fltemp_c1, cloud1, humidity1
+    if json2 is not None and json1 is None:
+        json2_curr = json2["days"][0]
+        temp_c2 = fahrenheit_to_celsius(json2_curr["temp"])
+        fltemp_c2 = fahrenheit_to_celsius(json2_curr["feelslike"])
+        humidity2 = json2_curr["humidity"]
+        cloud2 = json2_curr["cloudcover"]
+        return temp_c2, fltemp_c2, cloud2, humidity2
 
-    temp_c1 = json1["current"]["temp_c"]
-    temp_c2 = fahrenheit_to_celsius(json2["days"][0]["temp"])
-
-    fltemp_c1 = json1["current"]["feelslike_c"]
-    fltemp_c2 = fahrenheit_to_celsius(json2["days"][0]["feelslike"])
-
-    humidity1 = json1["current"]["humidity"]
-    humidity2 = json2["days"][0]["humidity"]
-
-    cloud1 = json1["current"]["cloud"]
-    cloud2 = json2["days"][0]["cloudcover"]
+    json1_curr = json1["current"]
+    json2_curr = json2["days"][0]
+    temp_c1 = json1_curr["temp_c"]
+    fltemp_c1 = json1_curr["feelslike_c"]
+    humidity1 = json1_curr["humidity"]
+    cloud1 = json1_curr["cloud"]
+    temp_c2 = fahrenheit_to_celsius(json2_curr["temp"])
+    fltemp_c2 = fahrenheit_to_celsius(json2_curr["feelslike"])
+    humidity2 = json2_curr["humidity"]
+    cloud2 = json2_curr["cloudcover"]
 
     temp_avg = (temp_c1 + temp_c2) / 2
     fltemp_avg = (fltemp_c1 + fltemp_c2) / 2
     humidity = (humidity1 + humidity2) / 2
     cloud = max(cloud1, cloud2)
-
     temp_avg, fltemp_avg, humidity = round_data([temp_avg, fltemp_avg, humidity])
-
     return temp_avg, fltemp_avg, cloud, humidity
 
 
 def get_current_extended_weather(city: str):
-    yesterday = datetime.now() - timedelta(days=1)
+    yesterday = datetime.now() - timedelta(days=2)
     yesterday_formatted = yesterday.strftime("%Y-%m-%d")
 
     json1 = fetch_data('http://api.weatherapi.com/v1/history.json?key=' + key1 + '&q=' + city + '&dt=' + yesterday_formatted)
     json2 = fetch_data('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + city + '?unitGroup=us&key=' + key2 + '&contentType=json')
+
+    if json1 is None or json2 is None:
+        raise requests.HTTPError("Unable to catch the data from an API")
 
     is_mud = json1["forecast"]["forecastday"][0]["day"]["daily_will_it_rain"]
     is_mud = bool(is_mud)
@@ -72,6 +87,8 @@ def get_current_extended_weather(city: str):
 def get_forecast_weather(city: str, days: int):
     json = fetch_data('http://api.weatherapi.com/v1/forecast.json?key=' + key1 + '&q=' + city + '&days=' + str(
         days + 1) + '&aqi=no&alerts=no')
+    if json is None:
+        raise requests.HTTPError("Unable to catch the data from an API")
 
     day = [None for _ in range(days)]
     for i in range(days):
